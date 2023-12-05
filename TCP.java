bdd547adc9a7a031218c697ddde4350014ca537e
ShiweiHe0713 <67429121+ShiweiHe0713@users.noreply.github.com>
2023-12-04 20:48:22 -0500

    updated the input part

--- TCP.java
+++ TCP.java
@@ -2,6 +2,48 @@ package Fsm;
 import java.util.Scanner;
 
 public class TCP {
+    public static void main(String[] args) {
+        TCP tcp = new TCP();
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNext()) {
+            Event event = new Event(scanner.next());
+            tcp.handleEvent(event.event_name);
+        }
+    }
+
+    public void handleEvent(String event) {
+        switch (event) {
+            case "PASSIVE":
+                System.out.println("it is PASSIVE");
+                // passiveOpen();
+                break;
+            case "ACTIVE":
+                // activeOpen();
+                break;
+            case "SYN":
+
+                break;
+            case "SYNACK":
+                break;
+            case "ACK":
+                break;
+            case "RDATA":
+                break;
+            case "SDATA":
+                break;
+            case "FIN":
+                break;
+            case "CLOSE":
+                break;
+            case "TIMEOUT":
+                break;
+            default:
+                System.out.println("Invalid Input: " + event);
+                break;
+        }
+    }
+
     class FSM { 
         String fsmName;
         State starState;
@@ -62,7 +104,7 @@ public class TCP {
         TIMEOUT
     }
     
-    class Event {
+    static class Event {
         // Data members
         public String event_name;
         public Object object;
@@ -85,21 +127,28 @@ public class TCP {
         public void setValue(Object o) {
             object = o;
         }
-        public String toString() {
-            // Return a String representing the state of the Event Object.
-            // Overrides: toString in class java.lang.Object
-        }
+        // public String toString() {
+        //     // Return a String representing the state of the Event Object.
+        //     // Overrides: toString in class java.lang.Object
+        // }
     }
+
 
     abstract class Action {
         public Action() {}
         public void execute(FSM f, Event e) {
+            // This method is passed the FSM and Event that caused the Transition. 
+            // The Action.execute() method may access the FSM to get current State 
+            // and then retrieve any persistent information associated with that State.
             // This method is invoked whenever the corresponding transition occurs. 
             // You may access the FSM to get current State and then 
             // retrieve any persistent information associated with that State.
             f.doEvent(e);
         }
     }
+
+
+
 }
 
 class State {
